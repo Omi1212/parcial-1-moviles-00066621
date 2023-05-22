88e69c92e5dad24a6585b276d65fa71b20726654
Omi1212 <000666621@uca.edu.sv>
2023-05-22 14:02:32 -0600

    add Model

---
+++ Parcial1/app/src/main/java/com/oosca/parcial1/data/DummyData.kt
@@ -0,0 +1,10 @@
+package com.oosca.parcial1.data
+
+import com.oosca.parcial1.data.model.NewspaperModel
+
+val name = "El Calentamiento Global"
+val publicationDate = "12/05/2021"
+
+val newspapers = mutableListOf(
+    NewspaperModel(name, publicationDate)
+)
---
+++ Parcial1/app/src/main/java/com/oosca/parcial1/data/model/NewspaperModel.kt
@@ -0,0 +1,8 @@
+package com.oosca.parcial1.data.model
+
+data class NewspaperModel(
+    val name: String,
+    val publicationDate: String,
+
+)
+
